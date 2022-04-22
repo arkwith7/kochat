b5dacc9bb40ba2a83e4e5ce713f099978a264044
arkwith7 <arkwith7@gmail.com>
2022-04-22 09:58:09 +0900

    init_20220421

--- demo/application.py
+++ demo/application.py
@@ -3,6 +3,12 @@
 @since 7/1/2020
 @see https://github.com/gusdnd852
 """
+
+# 소스다운 받은 위치를 고려해서 개발환경 구성 할때마다 추가 필요
+import sys
+sys.path.append('/home/diquest/chatbot/kochat')
+
+
 from flask import render_template
 
 from kochat.app import KochatApi
@@ -15,6 +21,10 @@ from demo.scenario import dust, weather, travel, restaurant
 # from scenario import dust, weather, travel, restaurant
 # 에러 나면 이걸로 실행해보세요!
 
+import time
+import datetime
+
+start = time.time()
 
 dataset = Dataset(ood=True)
 emb = GensimEmbedder(model=embed.FastText())
@@ -39,6 +49,12 @@ kochat = KochatApi(
     ]
 )
 
+end = time.time()
+
+sec = (end - start) 
+result_list = str(datetime.timedelta(seconds=sec)).split(".") 
+print("초기 서버 시작하는데 소요된 시간 : ",result_list[0])
+
 
 @kochat.app.route('/')
 def index():
@@ -46,6 +62,11 @@ def index():
 
 
 if __name__ == '__main__':
+    
+
     kochat.app.template_folder = kochat.root_dir + 'templates'
     kochat.app.static_folder = kochat.root_dir + 'static'
-    kochat.app.run(port=8080, host='0.0.0.0')
+    # kochat.app.run(port=8080, host='0.0.0.0')
+    kochat.app.run(port=8080, host='127.0.0.1')
+
+
